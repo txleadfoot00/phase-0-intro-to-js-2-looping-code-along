90b13af6c06021442c81d8a80d96070062ab7527
txleadfoot00 <106290919+txleadfoot00@users.noreply.github.com>
2022-08-17 16:04:56 -0700

    commit tree 3dbf2402a8693c3bb2a15dcb16140faae179af43

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards(people, occasion) {
+    let cardGreetingArray = [], i, cardGreeting
+      for (let i = 0; i < people.length; i++) {
+      let cardGreeting = (`Thank you, ${people[i]}, for the wonderful ${occasion} gift!`)
+        cardGreetingArray.push(cardGreeting);
+      }
+    return cardGreetingArray;
+    }
+    
+    function countDown(number){
+    while (number >= 0) {
+        console.log (number--);
+    }}
+    
